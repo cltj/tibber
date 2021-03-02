14d3d2b05ad9c87a21bebe9ca4b1f87973e33b10
Thor Jarle Kinstad <tj@MacBook-Tj.local>
2021-03-02 08:29:48 +0700

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # the main python file
+import requests
+import config
+
+
+
+url = config.RESPONSE_URL
+payload="{\"query\":\"{ \\n    viewer { \\n        name\\n    }\\n}\",\"variables\":{}}"
+
+headers = {
+  'Authorization': config.ACCESS_TOKEN,
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response)
+print(response.text)
